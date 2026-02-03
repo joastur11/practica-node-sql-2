2893d795c88ed89326f5ad79bee845846491af68
Joaquin Stur <joaquinstur@gmail.com>
2026-02-03 11:23:22 -0300

    update controller sin placeholder

--- src/backend/controllers/auth.controller.ts
+++ src/backend/controllers/auth.controller.ts
@@ -1,6 +1,21 @@
 import type { Request, Response } from "express";
+import { registerService } from "../services/auth.services.js";
 
 export function register (req: Request, res: Response) {
-  console.log('register controller ok')
-  return res.send('register controller ok')
+  try {
+    const { email, password, name, surname } = req.body
+
+    registerService({ email, password, name, surname })
+
+    return res.status(200).json({ ok: 'todoi ok' })
+
+  } catch (error) {
+    console.error('error en el registro: ', error)
+
+    if (error instanceof Error){
+      return res.status(400).json ({ error: error.message })
+    }
+    
+    return res.status(400).json ({ error: 'Error en el registro' })
+  }
 }
