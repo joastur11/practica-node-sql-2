22efe6fb0511b4dfb080b5bc66f736d79f13c2c4
Joaquin Stur <joaquinstur@gmail.com>
2026-02-03 11:23:04 -0300

    typo in service

--- src/backend/services/auth.services.ts
+++ src/backend/services/auth.services.ts
@@ -7,12 +7,12 @@ export function registerService ({email, password, name, surname}: Register){
     throw new Error('invalid email')
   }
   if(!password || typeof password != "string"){
-    throw new Error('invalid email')
+    throw new Error('invalid password')
   }
   if(!name || typeof name != "string"){
-    throw new Error('invalid email')
+    throw new Error('invalid name')
   }
   if(!surname || typeof surname != "string"){
-    throw new Error('invalid email')
+    throw new Error('invalid surname')
   }
 }
