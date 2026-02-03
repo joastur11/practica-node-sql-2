b9cc943b14389d0609c7f5728d81668c34de3bfb
Joaquin Stur <joaquinstur@gmail.com>
2026-02-03 19:20:13 -0300

    service de register

--- src/backend/services/auth.services.ts
+++ src/backend/services/auth.services.ts
@@ -1,8 +1,9 @@
+import type { RowDataPacket } from "mysql2";
 import type { Register }  from "../types.js";
+import { pool } from "../db/connections.js";
+import bcrypt from 'bcrypt'
 
-
-
-export function registerService ({email, password, name, surname}: Register){
+export async function registerService ({email, password, name, surname}: Register){
   if(!email || typeof email != "string"){
     throw new Error('invalid email')
   }
@@ -11,8 +12,28 @@ export function registerService ({email, password, name, surname}: Register){
   }
   if(!name || typeof name != "string"){
     throw new Error('invalid name')
-  }
+  }  
   if(!surname || typeof surname != "string"){
     throw new Error('invalid surname')
+  }
+
+  const saltRounds = 10  
+  const hashedPassword = await bcrypt.hash(password, saltRounds)
+
+  try {
+    const [rows] = await pool.query<RowDataPacket[]>('SELECT user_id FROM users WHERE user_email = ?', [email])
+    if (rows.length > 0){
+      throw new Error('email already in use')
+    }
+    
+    const [result] = await pool.query('INSERT INTO users (email, name, surname, password_hash) VALUES (?, ?, ?, ?)', [email, name, surname, hashedPassword])
+
+    const insertId = (result as any).insertId
+
+    return insertId
+
+  } catch (error) {
+    console.error('Error fetching user', error)
+    throw new Error('Error fetching user')
   }
 }
