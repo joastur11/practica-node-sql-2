6aa929ba2ff47c95aab6877f27a16d9311eb0d89
Joaquin Stur <joaquinstur@gmail.com>
2026-02-04 13:08:24 -0300

    add type UserRow

--- src/backend/types.ts
+++ src/backend/types.ts
@@ -1,6 +1,11 @@
-export type Register = {
-  email: 'string',
-  password: 'string',
-  name: 'string',
-  surname: 'string'
+export type User = {
+  email: string,
+  password: string,
+  name: string,
+  surname: string
+}
+
+export type UserRow = {
+  user_id: number,
+  password_hash: string
 }
