f7656a56159a0cf38ac2beb18f4088238f56591f
Joaquin Stur <joaquinstur@gmail.com>
2026-02-04 13:08:42 -0300

    removes placeholder in route

--- src/backend/routes/login.ts
+++ src/backend/routes/login.ts
@@ -1,7 +1,8 @@
 import { Router } from "express";
+import { login } from "../controllers/auth.controller.js";
 
 const router = Router()
 
-router.post('/', /*login*/ )  // va metodo POST en vez de GET por seguridad, el GET va en el url
+router.post('/', login )  // va metodo POST en vez de GET por seguridad, el GET va en el url
 
 export default router
