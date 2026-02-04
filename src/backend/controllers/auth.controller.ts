f0a9580a20a538aa071990c8e52e14a87a63d2b8
Joaquin Stur <joaquinstur@gmail.com>
2026-02-04 11:38:29 -0300

    add status error if email in use
    to controller

--- src/backend/controllers/auth.controller.ts
+++ src/backend/controllers/auth.controller.ts
@@ -5,17 +5,20 @@ export async function register (req: Request, res: Response) {
   try {
     const { email, password, name, surname } = req.body
 
-    await registerService({ email, password, name, surname })
+    const newUserId = await registerService({ email, password, name, surname })
 
-    return res.status(201).json({ ok: 'todo ok' })
+    return res.status(201).json({ newUserId })
 
   } catch (error) {
     console.error('error en el registro: ', error)
-
+    
     if (error instanceof Error){
+      if (error.message === 'email already in use'){
+        return res.status(409).json ({ error: error.message })
+      }
       return res.status(400).json ({ error: error.message })
     }
-    
+        
     return res.status(400).json ({ error: 'Error en el registro' })
   }
 }
