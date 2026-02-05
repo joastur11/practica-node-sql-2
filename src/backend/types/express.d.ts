2fd21142c384b07f47d7dcdee900f36bdfce290b
Joaquin Stur <joaquinstur@gmail.com>
2026-02-05 13:20:59 -0300

    crea tipo global para token

--- src/backend/types/express.d.ts
+++ src/backend/types/express.d.ts
@@ -1,2 +1,11 @@
-import express from 'express'
+import type { Request } from 'express'
 
+declare global {
+  namespace Express {
+    interface Request {
+      userId?: number
+    }
+  }
+}
+
+// esto es para que el token jwt en el middleware pueda 'viajar' a las otras paginas
