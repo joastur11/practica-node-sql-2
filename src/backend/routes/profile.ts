44c3216854a1dcef66dfe82174bfd86db9eb0393
Joaquin Stur <joaquinstur@gmail.com>
2026-02-06 12:08:04 -0300

    hace ruta profile funcional
    con middleware

--- src/backend/routes/profile.ts
+++ src/backend/routes/profile.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
+import { profile } from "../controllers/auth.controller.js";
+import { authMiddleware } from "../middlewares/auth.middleware.js";
 
 
 const router = Router()
 
-router.get('/', /* controler */)
+router.get('/', authMiddleware, profile ) // middleware siempre antes que controller
 
 export default router
