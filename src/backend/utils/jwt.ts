8572318fc26355bdd9b1618ec5c92bed4c90924c
Joaquin Stur <joaquinstur@gmail.com>
2026-02-09 12:20:06 -0300

    crea funcion p/ generar refresh token

--- src/backend/utils/jwt.ts
+++ src/backend/utils/jwt.ts
@@ -9,3 +9,13 @@ export function tokenGenerator(userId: number) {
 
   return token
 }
+
+export function refreshTokenGenerator(userId: number) {
+  const token = jwt.sign(
+    { userId },
+    process.env.JWT_REFRESH_SECRET!,
+    { expiresIn: '15d' }
+  )
+
+  return token
+}
