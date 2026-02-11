e6d41a539e7cee933fb303579c98668c5cd1a478
Joaquin Stur <joaquinstur@gmail.com>
2026-02-11 11:07:32 -0300

    uses logout route on app

--- src/backend/app.ts
+++ src/backend/app.ts
@@ -2,6 +2,7 @@ import express from 'express'
 import registerRoutes from './routes/register.js'
 import loginRoutes from './routes/login.js'
 import refreshRoutes from './routes/refresh.js'
+import logoutRoutes from './routes/logout.js'
 
 const app = express()
 
@@ -16,6 +17,8 @@ app.use('/register', registerRoutes)
 app.use('/login', loginRoutes)
 
 app.use('/refresh', refreshRoutes)
+
+app.use('/logout', logoutRoutes)
 
 const PORT = 1234
 
