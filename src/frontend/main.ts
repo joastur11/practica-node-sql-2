f4e03984d1fb0869d3b2c40dc4c5eb67d06f6962
Joaquin Stur <joaquinstur@gmail.com>
2026-02-17 12:56:25 -0300

    dom constants

---
+++ src/frontend/main.ts
@@ -0,0 +1,13 @@
+const loginView = document.querySelector('#login-view')
+const profileView = document.querySelector('#profile-view')
+
+const emailInput = document.querySelector('#email')
+const passwordInput = document.querySelector('#password')
+const nameInput = document.querySelector('#name')
+const lastnameInput = document.querySelector('#lastname')
+
+const loginBtn = document.querySelector('#login-btn')
+const registerBtn = document.querySelector('#register-btn')
+const profielBtn = document.querySelector('#profile-btn')
+const logoutBtn = document.querySelector('#logout-btn')
+
