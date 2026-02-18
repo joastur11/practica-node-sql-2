332775498cf8e199fac9c2e7492658184bf11674
Joaquin Stur <joaquinstur@gmail.com>
2026-02-18 19:37:15 -0300

    logica en .js para probar

---
+++ src/frontend/main.js
@@ -0,0 +1,38 @@
+const loginView = document.querySelector('#login-view')
+const profileView = document.querySelector('#profile-view')
+
+const nameLabel = document.querySelector('#name-label')
+const lastnameLabel = document.querySelector('#lastname-label')
+
+const emailInput = document.querySelector('#email')
+const passwordInput = document.querySelector('#password')
+const nameInput = document.querySelector('#name')
+const lastnameInput = document.querySelector('#lastname')
+
+const loginBtn = document.querySelector('#login-btn')
+const registerBtn = document.querySelector('#register-btn')
+const profileBtn = document.querySelector('#profile-btn')
+const logoutBtn = document.querySelector('#logout-btn')
+const createAccountBtn = document.querySelector('#create-account-btn')
+
+let isRegisterMode = false
+
+registerBtn?.addEventListener('click', () => {
+  isRegisterMode = !isRegisterMode
+
+  if (isRegisterMode) {
+    nameInput?.classList.remove("hidden")
+    lastnameInput?.classList.remove("hidden")
+    nameLabel?.classList.remove("hidden")
+    lastnameLabel?.classList.remove("hidden")
+    loginBtn?.classList.add("hidden")
+    createAccountBtn?.classList.remove("hidden")
+  } else {
+    nameInput?.classList.add("hidden")
+    lastnameInput?.classList.add("hidden")
+    nameLabel?.classList.add("hidden")
+    lastnameLabel?.classList.add("hidden")
+    loginBtn?.classList.remove("hidden")
+    createAccountBtn?.classList.add("hidden")
+  }
+})
