3afeea93df8159ab04ba3684a6bafe1751334626
Joaquin Stur <joaquinstur@gmail.com>
2026-02-23 13:11:34 -0300

    testea register

--- src/backend/tests/auth.test.ts
+++ src/backend/tests/auth.test.ts
@@ -11,14 +11,12 @@ import { loginService, registerService } from '../services/auth.services.js'
 describe('Auth endpoints', () => {
 
   // test de que devuelve 404 si alguien pone una ruta inexistente
-
   it('should return 404 on unknown route', async () => {
     const response = await request(app).get('/unknown-route')
     expect(response.status).toBe(404)
   })
 
   // test login
-
   describe('POST /login', () => {
     // credenciales validas
     it('should return 200 and jwt tokens when credentials are valid', async () => {
@@ -53,7 +51,6 @@ describe('Auth endpoints', () => {
   })
   
   // test register
-
   describe('POST /register', () => {
     // registro exitoso
     it('should return 201 and new user id if register was succesfull', async () =>{
@@ -71,9 +68,41 @@ describe('Auth endpoints', () => {
       expect(response.status).toBe(201)
       expect(response.body.newUserId).toBeDefined()
     })
-    
 
+    // error, email ya registrado
+    it('should return 409 if the email is already in use', async () => {
+      (registerService as jest.Mock).mockRejectedValue(new Error('email already in use'))
+
+      const response = await request(app)
+      .post('/register')
+      .send({
+        email: 'usedemail@asd.com',
+        password: 'asd',
+        name: 'asd',
+        lastname: 'asd'
+      })
+
+      expect(response.status).toBe(409)
+    })
+
+    // error, registro invalido
+    it ('should return 400 if the credentials are invalid', async () => {
+      (registerService as jest.Mock).mockRejectedValue(new Error('Error en el registro'))
+
+      const response = await request(app)
+      .post('/register')
+      .send({
+        email: 'usedemail@asd.com',
+        password: 'asd',
+        name: 'asd',
+        lastname: 'asd'
+      })
+
+      expect(response.status).toBe(400)
+    })
   })
 
+  // test refresh
+  
 
 })
